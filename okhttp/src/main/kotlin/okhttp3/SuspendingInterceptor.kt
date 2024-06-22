57f6fcaefff2f6253dd0d088cd3189d21e14a09c
Yuri Schimke <yuri@schimke.ee>
2024-06-22 17:33:13 +0100

    API

--- okhttp/src/main/kotlin/okhttp3/SuspendingInterceptor.kt
+++ okhttp/src/main/kotlin/okhttp3/SuspendingInterceptor.kt
@@ -1,13 +1,33 @@
 package okhttp3
 
 import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor.Chain
 
-abstract class SuspendingInterceptor: Interceptor {
-  final override fun intercept(chain: Interceptor.Chain): Response {
-    return runBlocking {
-      interceptAsync(chain)
+abstract class SuspendingInterceptor : Interceptor {
+    final override fun intercept(chain: Interceptor.Chain): Response {
+        return runBlocking {
+            interceptAsync(chain)
+        }
     }
-  }
 
-  abstract suspend fun interceptAsync(chain: Interceptor.Chain): Response
+    abstract suspend fun interceptAsync(chain: Interceptor.Chain): Response
+
+    companion object {
+        /**
+         * Constructs an interceptor for a lambda. This compact syntax is most useful for inline
+         * interceptors.
+         *
+         * ```kotlin
+         * val interceptor = SuspendingInterceptor { chain: Interceptor.Chain ->
+         *     chain.proceedAsync(chain.request())
+         * }
+         * ```
+         */
+        inline operator fun invoke(crossinline block: suspend (chain: Chain) -> Response): SuspendingInterceptor =
+            object : SuspendingInterceptor() {
+                override suspend fun interceptAsync(chain: Chain): Response {
+                    return block(chain)
+                }
+            }
+    }
 }
